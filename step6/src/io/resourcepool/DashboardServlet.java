9a1394749a6dca5d4be5b6241915d82554a99cac
loic ortola <contact@loicortola.com>
2017-09-05 20:00:02 +0200

    Added codebase for step6. Needs improvements

---
+++ step6/src/io/resourcepool/DashboardServlet.java
@@ -0,0 +1,24 @@
+package io.resourcepool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/dashboard")
+public class DashboardServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("liveUserCount", getServletContext().getAttribute("liveUserCount"));
+		request.getRequestDispatcher("WEB-INF/dashboard.jsp").forward(request, response);
+	}
+}
---
+++ step6/src/io/resourcepool/UserDao.java
@@ -0,0 +1,27 @@
+package io.resourcepool;
+
+import java.util.List;
+
+import javax.ejb.Singleton;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Singleton
+public class UserDao {
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	public void save(User u) {
+		em.persist(u);
+	}
+	
+	public User findOne(Long id) {
+		return em.find(User.class, id);
+	}
+	
+	public List<User> findAll() {
+		return em.createQuery("SELECT * FROM user").getResultList();
+	}
+
+}
